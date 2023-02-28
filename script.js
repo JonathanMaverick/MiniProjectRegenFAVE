106b641068169b6d054984a20aeef5450f3e2dec
JonathanMaverick <jonathanmaverick.jm@gmail.com>
2023-02-28 11:00:39 +0700

    first commmit

---
+++ script.js
@@ -0,0 +1,52 @@
+let inputNameValue = document.getElementById("name");
+let inputEmailValue = document.getElementById("email");
+let inputTelephoneValue = document.getElementById("phone");
+let inputMessageValue = document.getElementById("messageBox");
+let error = document.getElementById("error");
+
+
+document.getElementById("forms").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    error.innerText = '';
+
+    if(inputNameValue.value === ""){
+        error.innerText = 'Please enter your name';
+        return;
+    } else if(inputNameValue.value.length < 3){
+        error.innerText = 'Name must be more than 3 characters';
+        return;
+    }
+
+    if(inputEmailValue.value === ""){
+        error.innerText = 'Please enter your email';
+        return;
+    } else if(inputEmailValue.value.split("@").length === 1 || inputEmailValue.value.split("@").length > 2){
+        error.innerText = 'Email must contain one @';
+        return;
+    } else if(!inputEmailValue.value.split("@")[1].includes(".com")){
+        error.innerText = 'Email must contain .com after @';
+        return;
+    }
+
+    if(inputTelephoneValue.value == "") 
+    {
+        error.innerText = "Phone number must exist!";
+        return;
+    }else if(inputTelephoneValue.value[0] != '0' || inputTelephoneValue.value[1] != '8')
+    {
+        error.innerText = "Phone number start with 08!";
+        return;
+    }
+
+    if(inputMessageValue.value === ""){
+        error.innerText = 'Message cannot be blank';
+        return;
+    } else if(inputMessageValue.value.split(' ').length < 4){
+        error.innerText = 'Message must contain more than 3 words';
+        return;
+    }
+
+
+    document.getElementById("forms").submit();
+});
